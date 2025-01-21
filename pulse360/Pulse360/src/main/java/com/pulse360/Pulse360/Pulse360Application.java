ecc4c04a69726295e69ac7824348d614ed57fb2e
Satwik Kar <satwik.k.2000@gmail.com>
2025-01-21 22:15:33 +0530

    project setup

---
+++ pulse360/Pulse360/src/main/java/com/pulse360/Pulse360/Pulse360Application.java
@@ -0,0 +1,13 @@
+package com.pulse360.Pulse360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pulse360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pulse360Application.class, args);
+	}
+
+}
---
+++ pulse360/Pulse360/src/test/java/com/pulse360/Pulse360/Pulse360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulse360.Pulse360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pulse360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
